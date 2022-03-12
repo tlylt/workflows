db29ba9c4e756f877ddec90473e6c88d29513f16
tlylt <liu_yongliang@hotmail.com>
2022-03-12 22:35:10 +0800

    add shell

---
+++ scripts/sample.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Start shell"
+ls -a
+
+# String
+if [[ -z "$string" ]]; then
+  echo "String is empty"
+elif [[ -n "$string" ]]; then
+  echo "String is not empty"
+else
+  echo "This never happens"
+fi
