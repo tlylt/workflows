caebd380fddf136d92c58c72f3ea8a007c4f1a2d
tlylt <liu_yongliang@hotmail.com>
2022-03-29 10:57:43 +0800

    Update bot

--- telegram_bots/reminder_gw_bot/bot.py
+++ telegram_bots/reminder_gw_bot/bot.py
@@ -1,6 +1,7 @@
 from telegram.ext import Updater, CommandHandler
 import os
 from dotenv import load_dotenv, find_dotenv
+import requests
 from command_handlers import (
     help_command_handler,
 )
@@ -18,8 +19,11 @@ def main():
     dp.add_handler(CommandHandler("help", help_command_handler))
     dp.add_handler(CommandHandler("start", help_command_handler))
 
+    # get quote
+    response = requests.get("https://quotes.rest/qod?language=en")
+    msg = response.json().get("contents").get("quotes")[0].get("quote")
     # send chat
-    dp.bot.send_message(os.environ.get("MY_CHAT_ID", ""), "Hello World!")
+    dp.bot.send_message(os.environ.get("MY_CHAT_ID", ""), msg)
     print("DONE!")
 
 if __name__ == "__main__":
